0d3f76d6089188065cd04354f3480c9efacab173
Richardzejak <Richardzejak@hotmail.com>
2021-11-03 13:37:27 +0100

    simple about page

--- page-om.php
+++ page-om.php
@@ -1,9 +1,37 @@
 <?php
 
+$title = get_field('title');
+$desc = get_field('description');
+
+$image = get_field('image');
+$theImage = $image['sizes']['medium'];
+
+
 get_header();
 
 ?>
-<h1>this is page: Om</h1>
+
+<div class="container">
+    <div class="d-flex about-column">
+        <div class="p-2 about-text-container">
+            <h4> <?php echo $title ?></h4>
+            <h2> <?php echo $desc ?></h2>
+        </div>
+        <div class="p-2 about-image-container">
+            <div>
+                <img class="about-image" src="<?php echo $theImage; ?>">
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
 <?php
 
 get_footer();
