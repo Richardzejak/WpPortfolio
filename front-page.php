57c65a7865ee342ad4d67f910664f7ec0ede2859
Richardzejak <Richardzejak@hotmail.com>
2021-11-03 12:28:49 +0100

    made all values dynamical from advance custom fields

--- front-page.php
+++ front-page.php
@@ -1,7 +1,15 @@
 <?php
 
+$title = get_field('title');
+$desc = get_field('description');
+$desc2 = get_field('description2');
+
 $image = get_field('image');
 $theImage = $image['sizes']['medium'];
+
+$linkedin = get_field('linkedin');
+$github = get_field('github');
+$mail = get_field('mail');
 
 get_header();
 
@@ -10,9 +18,9 @@ get_header();
 <div class="container">
     <div class="d-flex frontpage-column">
         <div class="p-2 frontpage-text-container">
-            <h4> Hej mitt namn är Richard Zejak.</h4>
-            <h2> Du kommer med idéer jag, färdigställer dom.</h2>
-            <h4> Jag är en svensk webbutvecklare som älskar att vara kreativ.</h4>
+            <h4> <?php echo $title ?></h4>
+            <h2> <?php echo $desc ?></h2>
+            <h4> <?php echo $desc2 ?></h4>
             <a class="frontpage-link" href="om">Mer Om mig ></a>
         </div>
         <div class="p-2 frontpage-image-container">
@@ -20,9 +28,9 @@ get_header();
                 <img class="frontpage-image" src="<?php echo $theImage; ?>">
             </div>
             <div class="frontpage-icon-container">
-                <a class="bi bi-linkedin" href="#" target="_blank"></a>
-                <a class="bi bi-github" href="#" target="_blank"></a>
-                <a class="bi bi-mailbox2" href="#"></a>
+                <a class="bi bi-linkedin" href="<?php echo $linkedin ?>" target="_blank"></a>
+                <a class="bi bi-github" href="<?php echo $github ?>" target="_blank"></a>
+                <a class="bi bi-mailbox2" href="mailto: <?php echo $mail ?>"></a>
                 <a class="bi bi-geo-alt-fill" href="#"></a>
             </div>
         </div>
